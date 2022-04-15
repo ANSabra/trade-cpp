421a3b542b51eb79ec37af02d1aecf7e9e2f29a2
ANSabra <103777619+ANSabra@users.noreply.github.com>
2022-04-16 01:14:53 +0200

    Add vcpkg, boost dependency and SonarCloud analysis
    Add boost
    
    add vcpkg manifest
    
    checkout submodule
    
    another try
    
    build using vcpkg action
    
    Add SonarCloud analysis
    
    fix yml
    
    restrict the scope of boost

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
+#include <boost/algorithm/algorithm.hpp>
 
 int main() {
-  std::cout << "Hello, trade-cpp";
+  std::cout << "Hello, trade-cpp:" << boost::algorithm::power(2, 2);
 }
